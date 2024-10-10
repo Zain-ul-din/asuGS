cf671e3fc490e7f01394d65ae4b1eb0c1a3d2bc4
zainuldeen <78583049+Zain-ul-din@users.noreply.github.com>
2024-10-10 14:36:15 +0500

    refactor: move code to own files (#2)

---
+++ lib/app.dart
@@ -0,0 +1,65 @@
+import 'package:asugs/services/auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        StreamProvider<User?>(
+            create: (ctx) => AuthService().getAuth().userChanges(),
+            initialData: null)
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+          useMaterial3: true,
+        ),
+        home: HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Hello world"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  AuthService().googleSignIn();
+                },
+                child: const Text("SignIn With Google")),
+            ElevatedButton(
+              onPressed: () {
+                AuthService().signOut();
+              },
+              child: const Text("SignOut"),
+            )
+          ],
+        ),
+      ),
+      // floatingActionButton: FloatingActionButton(
+      //   onPressed: _incrementCounter,
+      //   tooltip: 'Increment',
+      //   child: const Icon(Icons.add),
+      // ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
